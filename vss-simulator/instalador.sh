1ee93a090953b699fff4b799be7f33e473c08048
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-07-23 00:09:21 -0300

    Implementação do script instalador
    O script anterior, escrito em C++, gerenciava a instalação dos módulos do projeto. A transformação do programa em apenas um script simplifica a manutenção do código (menos chamadas diretamente ao sistema, maior portabilidade). Além disso, por ser um script bash, não é necessário recompilar o instalador caso haja a necessidade de mudança de parte do sistema.

---
+++ vss-simulator/instalador.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+declare -a StringArray=("VSS-Core" "VSS-Simulator" "VSS-Viewer" "VSS-SampleCpp" "../VSSSERUS")
+
+declare -a BoolArray=(false false false false false)
+
+MAIN() {
+	clear
+
+	echo -e "Instalador VSS-Simulator\n"
+	echo -e "Simulador by Rodetas e RobôCIn\n"
+	echo -e "Script de instalação desenvolvido por ERUS\n"
+
+	echo -e "Requisitos:\nCMAKE, git e boost instalados\n"
+
+	echo -e "Lembre-se de rodar esse script com sudo!\n"
+
+	echo -e "Pressione enter para continuar..."
+
+	read
+
+	clear
+
+	echo "Deseja instalar o VSS-Simulator? (S/N)"
+
+	read ans_inst
+
+	if [ "$ans_inst" = "S" ] || [ "$ans_inst" = "s" ]; then
+		echo
+	else
+		echo -e "Finalizando..."
+		return 0
+	fi
+
+	clear
+
+	ASK_INSTALL;
+
+	DO_INSTALL;
+
+	return 0
+}
+
+# Pergunta ao usuário quais sistemas ele deseja instalar
+ASK_INSTALL() {
+	declare iterator=0
+
+	for val in ${StringArray[@]}; do
+		echo "Deseja instalar $val?(S/N) - $iterator"
+		read ans
+		if [ "$ans" = "S" ] || [ "$ans" = "s" ]; then
+			BoolArray[iterator]=true
+			echo "install"
+		else
+			echo
+		fi
+		iterator=$((iterator+1))
+		clear
+	done
+}
+
+
+# Realiza a instalação de fato - TODO
+DO_INSTALL() {
+	for val in ${BoolArray[@]}; do
+		echo "$val"
+	done
+}
+
+MAIN;
