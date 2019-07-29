851e13d2a24351d318d3a5ad87f3306f4d81b589
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-07-29 14:14:32 -0300

    WIP: Atualização da documentação da classe Utils
    Atualização da classe Utils para refletir os padrões de código do projeto

--- src/utils.h
+++ src/utils.h
@@ -8,15 +8,13 @@
 namespace Utils {
 
     /**
-     * @brief Estrutura para guardar as informações de uma posição.
-     * 
+     * Estrutura para guardar as informações de uma posição.
+     *
      */
-    typedef struct Posture
-    {
+    typedef struct Posture {
         double x, y, angle;
 
-        Posture(double x, double y, double angle)
-        {
+        Posture(double x, double y, double angle) {
             this->x = x;
             this->y = y;
             this->angle = angle;
@@ -24,45 +22,32 @@ namespace Utils {
 
     } Posture;
 
-/**
-     * @brief Translate a angle from [-PI,PI] to [0, 2*PI]
-     * 
-     * @param angle 
-     * @return double 
+    /**
+     * Transforma um ângulo de [-PI, PI] para [0, 2PI]
+     *
      */
-double toPositiveAngle(double angle)
-{
-    return fmod(angle + 2 * M_PI, 2 * M_PI);
+    double toPositiveAngle(double angle) {
+        return fmod(angle + 2 * M_PI, 2 * M_PI);
     }
 
     /**
-     * @brief Return the minimun angle distance between two angles
-     * 
-     * @param target 
-     * @param source 
-     * @return double 
+     * @brief Retorna a menor distância angular entre dois ângulos
+     *
      */
-    double smallestAngleDiff(double target, double source)
-    {
+    double smallestAngleDiff(double target, double source) {
         double a = Utils::toPositiveAngle(target) - Utils::toPositiveAngle(source);
-
-        if (a > M_PI)
-        {
+        if (a > M_PI) {
             a = a - 2 * M_PI;
         }
-        else if (a < -M_PI)
-        {
+        else if (a < -M_PI) {
             a = a + 2 * M_PI;
         }
-
         return a;
     }
 
     /**
-     * @brief Transform an angle to the range [-PI, PI]
-     * 
-     * @param angle 
-     * @return double 
+     * Transforma um ângulo para o domínio [-PI, PI]
+     *
      */
     double to180range(double angle) {
         angle = fmod(angle, 2 * M_PI);
@@ -75,23 +60,16 @@ double toPositiveAngle(double angle)
     }
 
     /**
-     * @brief Return the distance between two points
-     * 
-     * @param first 
-     * @param second 
-     * @return double 
+     * Retorna a distância entre dois pontos
+     *
      */
     double distance(doublePair first, doublePair second) {
         return std::sqrt(std::pow(first.first - second.first, 2) + std::pow(first.second - second.second, 2));
     }
 
     /**
-     * @brief Transform value x in between low and high
-     * 
-     * @param x 
-     * @param low 
-     * @param high 
-     * @return double 
+     * @brief Transforma o valor x para o domínio [low, high]
+     *
      */
     double bound (double x, double low, double high) {
         if (x < low) x = low;
