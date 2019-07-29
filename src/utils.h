00cca3ae114d415b6f26e3f2a66adbc0bdd1e165
Mayke Wallace <mayke.ace@hotmail.com>
2019-07-29 16:25:30 -0300

    Atualização da documentação
    Documentação da classe utils.h atualizada para seguir os padrões do projeto.

--- src/utils.h
+++ src/utils.h
@@ -23,17 +23,29 @@ namespace Utils {
     } Posture;
 
     /**
-     * Transforma um ângulo de [-PI, PI] para [0, 2PI]
-     *
-     */
+    * Nome da função:       toPositiveAngle;
+    * Intenção da função:   Transforma um ângulo de [-PI, PI] para [0, 2PI];
+    * Pré-requisitos:       O angulo tem que está entre [-PI, PI];
+    * Efeitos colaterais:   Não há;
+    * Parâmetros:           (double) angle: ângulo a ser transformado;
+    * Retorno:              (double) angle: ângulo transformado;
+    */
+
     double toPositiveAngle(double angle) {
         return fmod(angle + 2 * M_PI, 2 * M_PI);
     }
 
-    /**
-     * @brief Retorna a menor distância angular entre dois ângulos
-     *
-     */
+
+        /**
+        * Nome da função:       smallestAngleDiff;
+        * Intenção da função:   Retorna a menor distância angular entre dois ângulos;
+        * Pré-requisitos:       Necessitar da diferença entre dois ângulos;
+        * Efeitos colaterais:   Não há;
+        * Parâmetros:           (double) target: ângulo alvo;
+        *                       (double) source: ângulo fonte;
+        * Retorno:              (double) a: distancia ângular entre os dois ângulos;
+        */
+
     double smallestAngleDiff(double target, double source) {
         double a = Utils::toPositiveAngle(target) - Utils::toPositiveAngle(source);
         if (a > M_PI) {
@@ -46,9 +58,14 @@ namespace Utils {
     }
 
     /**
-     * Transforma um ângulo para o domínio [-PI, PI]
-     *
-     */
+    * Nome da função:       to180range;
+    * Intenção da função:   Transforma um ângulo para o domínio [-PI, PI];
+    * Pré-requisitos:       O angulo está num domínio diferente de [-PI, PI];
+    * Efeitos colaterais:   Não há;
+    * Parâmetros:           (double) angle: ângulo ser transformado;
+    * Retorno:              (double) angle: ângulo transformado;
+    */
+
     double to180range(double angle) {
         angle = fmod(angle, 2 * M_PI);
         if (angle < - M_PI) {
@@ -60,17 +77,30 @@ namespace Utils {
     }
 
     /**
-     * Retorna a distância entre dois pontos
-     *
-     */
+    * Nome da função:       distance;
+    * Intenção da função:   Retorna a distância entre dois pontos;
+    * Pré-requisitos:       Necessitar da diferença entre dois ângulos;
+    * Efeitos colaterais:   Não há;
+    * Parâmetros:           (double) first: primeiro ângulo;
+    *                       (double) second: primeiro ângulo;
+    * Retorno:              Um numero (double), que é a distância entre o first e o second;
+    */
+
     double distance(doublePair first, doublePair second) {
         return std::sqrt(std::pow(first.first - second.first, 2) + std::pow(first.second - second.second, 2));
     }
 
     /**
-     * @brief Transforma o valor x para o domínio [low, high]
-     *
-     */
+    * Nome da função:       bound;
+    * Intenção da função:   Transforma o valor x para o domínio [low, high];
+    * Pré-requisitos:       traduzir x em um valor high ou low;
+    * Efeitos colaterais:   Não há;
+    * Parâmetros:           (double) x: numero a ser transformado;
+    *                       (double) low: numero de comparação;
+    *                       (double) high: numero de comparação;
+    * Retorno:              (double) x: numero transformado;
+    */
+
     double bound (double x, double low, double high) {
         if (x < low) x = low;
         if (x > high) x = high;
