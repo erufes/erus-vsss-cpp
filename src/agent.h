1446e3a14e03776081461602dd7eede7327ed34d
Mayke Wallace <mayke.ace@hotmail.com>
2019-07-31 17:28:29 -0300

    Fora adicionado comentarios as funções de acordo com a documentação padrão, faltou unicamente comentar a função distancia.

--- src/agent.h
+++ src/agent.h
@@ -10,6 +10,14 @@
  * Membros: Ricardo Ramos, Gabriel Valdino
  */
 
+ /* Nome da função
+  * Intenção da função:
+  * Pré-requisitos:
+  * Efeitos colaterais:
+  * Parâmetros:
+  * Retorno:
+  */
+
 #include "ponto.h"
 #include <vector>
 #include <cstdlib>
@@ -26,50 +34,57 @@ namespace vsssERUS{
 		Agent(Ponto ponto);
 		virtual ~Agent(){};
 
-		/* getxyOld
-		* Intenção da função:
-		* Pré-Requisitos:
-		* Efeitos colaterais:
-		* Parâmetros:
-		* Retorno:
-		*/
+	  	/**
+  		* Nome da função:       getxyOld;
+  		* Intenção da função:   Retorna as posições anteriores;
+  		* Pré-Requisitos:       Não há;
+  		* Efeitos colaterais:   Não há;
+  		* Parâmetros:           Não há;
+  		* Retorno:						  (vector<Ponto>) posicoesAnteriores: localização anterior;
+  		*/
 		vector<Ponto> getxyOld() const;
 
-		/* previsaoDePosicao
-		* Intenção da função:
-		* Pré-Requisitos:
-		* Efeitos colaterais:
-		* Parâmetros:
-		* Retorno:
-		*/
+		  /**
+		  * Nome da função:       previsaoDePosicao;
+		  * Intenção da função:   Faz a previsão de uma futura posição da bola/player;
+		  * Pré-Requisitos:       Não há;
+		  * Efeitos colaterais:   Não há;
+		  * Parâmetros:           Não há;
+		  * Retorno:						  pair<double, double> ponto_futuro: localização futura;
+		  */
 		virtual std::pair<double, double> previsaoDePosicao() const;
 
-		/* updatePosition
-		* Intenção da função:
-		* Pré-Requisitos:
-		* Efeitos colaterais:
-		* Parâmetros:
-		* Retorno:
-		*/
+  		/**
+  		* Nome da função:       updatePosition
+  		* Intenção da função:   Atualização da posição do robô;
+		  * Pré-Requisitos:       Não há;
+		  * Efeitos colaterais:   Não há;
+		  * Parâmetros:           ((double) x:posição, (double) y: posição) ou ((Ponto)ponto: vetor de posição);
+		  * Retorno:						  void: são atualizados os vetores de posição por meio da função sem necessariamente precisar de um retorno;
+		  */
 		void updatePosition(double x, double y);
 		void updatePosition(Ponto ponto);
 
-		/* getPosicao
-		* Intenção da função:
-		* Pré-Requisitos:
-		* Efeitos colaterais:
-		* Parâmetros:
-		* Retorno:
-		*/
+		  /**
+	  	* Nome da função:       getPosicao;
+	  	* Intenção da função:   Função que chama a posição atual;
+  		* Pré-Requisitos:       Não há;
+	  	* Efeitos colaterais:   Não há;
+	  	* Parâmetros:           Não há;
+	  	* Retorno:						  (Ponto) posição: posição atual;
+  		*/
+
 		Ponto getPosicao() const;
 
-		/* distancia
-		* Intenção da função:
-		* Pré-Requisitos:
-		* Efeitos colaterais:
-		* Parâmetros:
-		* Retorno:
-		*/
+  		/**
+  		* Nome da função:       distancia;
+  		* Intenção da função:   Função que chama a posição atual;
+  		* Pré-Requisitos:       Não há;
+  		* Efeitos colaterais:   Não há;
+  		* Parâmetros:           Não há;
+  		* Retorno:						  (Ponto) posição: posição atual;
+  		*/
+
 		float distancia(Agent& agente) const;
 	};
 }
