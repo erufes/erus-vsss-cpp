4ec88a39cba16e87db989689820b38bde7e0720c
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-10-02 15:53:04 -0300

    Atualização do Ponto (Namespaces)

--- src/ponto.h
+++ src/ponto.h
@@ -14,72 +14,73 @@
 #define DOUBLE_CMP_THRESHOLD 0.000000000000001
 
 using namespace std;
-	namespace vsssERUS{
-	class Ponto
-	{
-		double x;
-		double y;
-	public:
-		Ponto(double x = 0, double y = 0);
 
-		/* distancia
-		* Inten��o da fun��o:
-		* Pr�-requisitos:
-		* Efeitos colaterais:
-		* Parametros:
-		* Retorno:
-		*/
-		double distancia(Ponto outro) const;
+namespace vsssERUS {
+class Ponto
+{
+	double x;
+	double y;
+public:
+	Ponto(double x = 0, double y = 0);
 
-		/* getX
-		* Inten��o da fun��o:
-		* Pr�-requisitos:
-		* Efeitos colaterais:
-		* Parametros:
-		* Retorno:
-		*/
-		double getX() const;
+	/* distancia
+	* Inten��o da fun��o:
+	* Pr�-requisitos:
+	* Efeitos colaterais:
+	* Parametros:
+	* Retorno:
+	*/
+	double distancia(Ponto outro) const;
 
-		/* getY
-		* Inten��o da fun��o:
-		* Pr�-requisitos:
-		* Efeitos colaterais:
-		* Parametros:
-		* Retorno:
-		*/
-		double getY() const;
+	/* getX
+	* Inten��o da fun��o:
+	* Pr�-requisitos:
+	* Efeitos colaterais:
+	* Parametros:
+	* Retorno:
+	*/
+	double getX() const;
 
-		/* setX
-		* Inten��o da fun��o:
-		* Pr�-requisitos:
-		* Efeitos colaterais:
-		* Parametros:
-		* Retorno:
-		*/
-		void setX(double x);
+	/* getY
+	* Inten��o da fun��o:
+	* Pr�-requisitos:
+	* Efeitos colaterais:
+	* Parametros:
+	* Retorno:
+	*/
+	double getY() const;
 
-		/* setY
-		* Inten��o da fun��o:
-		* Pr�-requisitos:
-		* Efeitos colaterais:
-		* Parametros:
-		* Retorno:
-		*/
-		void setY(double y);
+	/* setX
+	* Inten��o da fun��o:
+	* Pr�-requisitos:
+	* Efeitos colaterais:
+	* Parametros:
+	* Retorno:
+	*/
+	void setX(double x);
 
-		/* resolucaoDeSistemaLinear
-		* Inten��o da fun��o:
-		* Pr�-requisitos:
-		* Efeitos colaterais:
-		* Parametros:
-		* Retorno:
-		*/
-		//Resolu��o do sistema Ax = b, sendo A = matrix, b = vetor, N = ordem de matrix
-		//Aten��o � chamada da fun��o, pois ela precisa de um "double** A" e n�o um "double A[N][N]", logo, � necess�rio o uso de mallocs
-		//Cr�ditos � professora Cl�udia Galarda Varassin pela cria��o da fun��o que foi poucamente modificada para atender � nossos anseios
-		static vector<double> resolucaoDeSistemaLinear(double** matrix, double* vetor, int N);
+	/* setY
+	* Inten��o da fun��o:
+	* Pr�-requisitos:
+	* Efeitos colaterais:
+	* Parametros:
+	* Retorno:
+	*/
+	void setY(double y);
 
-		bool operator==(Ponto const &p);
-	};
-}
+	/* resolucaoDeSistemaLinear
+	* Inten��o da fun��o:
+	* Pr�-requisitos:
+	* Efeitos colaterais:
+	* Parametros:
+	* Retorno:
+	*/
+	//Resolu��o do sistema Ax = b, sendo A = matrix, b = vetor, N = ordem de matrix
+	//Aten��o � chamada da fun��o, pois ela precisa de um "double** A" e n�o um "double A[N][N]", logo, � necess�rio o uso de mallocs
+	//Cr�ditos � professora Cl�udia Galarda Varassin pela cria��o da fun��o que foi poucamente modificada para atender � nossos anseios
+	static vector<double> resolucaoDeSistemaLinear(double** matrix, double* vetor, int N);
+
+	bool operator==(Ponto const &p);
+};
+} // vsssERUS
 #endif // PONTO_H
