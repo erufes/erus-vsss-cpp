5c825c066a729290b6690aabdbaef9c238fc5107
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2019-10-04 18:25:40 -0300

    Inicio do Goleiro
    Inicio da implementação de Comportamento dos goleiros.

--- src/goleirobehavior.cpp
+++ src/goleirobehavior.cpp
@@ -6,18 +6,29 @@
  */
 
 #include "goleirobehavior.h"
+#include "world.h"
+#include <cmath>
 
 vsssERUS::GoleiroBehavior::GoleiroBehavior() {
 
 }
 
 vsssERUS::Ponto vsssERUS::GoleiroBehavior::movimenta(vsssERUS::Ponto posicao, vsssERUS::World* mundo){
-	//todo: implementar vsssERUS::GoleiroBehavior::movimenta
+	/* Ideia da implementação :
+	 * 	Posicionar o robô de forma que ele impessa a trajetória da bola
+	 * 		Como : 	Calcular o angulo de abertura entre a bola e o gol
+	 * 				Posicionar o robô onde o "triangulo" tenha base de 7,5 (tamanho do robô)
+	 */
+
+	//TODO: implementar vsssERUS::GoleiroBehavior::movimenta
+	Ponto ballPosition = (mundo->getBall()->getPosicao());
+	// a² = b² + c² - 2bc.cos α 
+	// float alfa = acos(thisThingUpHere);
 	return Ponto();
 }
 
 std::pair<int,int> vsssERUS::GoleiroBehavior::controle(vsssERUS::Ponto posicao, vsssERUS::World* mundo){
-	//todo: implementar vsssERUS::GoleiroBehavior::controle
+	//TODO: implementar vsssERUS::GoleiroBehavior::controle
 	return std::pair<int,int>();
 }
 
